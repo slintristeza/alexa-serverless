cda1b44cbb66728b6b165b272ed5e90f0e6a027e
slintristeza <slintristeza@gmail.com>
2021-03-19 21:39:41 +0900

    feat:speach Type of trash for the specified date

--- handler.ts
+++ handler.ts
@@ -1,6 +1,8 @@
 import * as Ask from 'ask-sdk';
 import 'source-map-support/register';
 
+import garbageCollectionSchedule from './csvUtils/garbageCollectionSchedule.json';
+
 export const alexa = Ask.SkillBuilders.custom()
   .addRequestHandlers({
     canHandle: (handlerInput) => {
@@ -22,9 +24,14 @@ export const alexa = Ask.SkillBuilders.custom()
     handle: (handlerInput: any) => {
       console.log(handlerInput);
       const intent = handlerInput.requestEnvelope.request.intent;
+      const targetDate = intent.slots.targetDate.value;
+      if (!targetDate) {
+        const speaachText = '日付が正しく指定されていません。もう一度やり直してください。'
+        return handlerInput.responseBuilder.speak(speaachText).getResponse();
+      }
       console.log(JSON.stringify(intent, null, 2));
-      const dayName = '日月火水木金土'[new Date(intent.slots.targetDate.value).getDay()];
-      const speaachText = `指定した日は${dayName}曜日です`;
+      const garbageType = garbageCollectionSchedule.filter(item => item['年月日'] === targetDate);
+      const speaachText = garbageType[0]['5'] ? `${targetDate}に収集されるゴミは${garbageType[0]['5']}です` : `${targetDate}のゴミの収集はありません` ;
       return handlerInput.responseBuilder.speak(speaachText).getResponse();
     },
   })
